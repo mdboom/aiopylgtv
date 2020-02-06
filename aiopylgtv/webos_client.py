7c6f216da849e09bafeaf4646bf2e4d4a5e3d121
Josh Bendavid <Josh.Bendavid@cern.ch>
2020-02-06 17:47:31 +0100

    remove message size limit on main connection

--- aiopylgtv/webos_client.py
+++ aiopylgtv/webos_client.py
@@ -167,6 +167,7 @@ class WebOsClient:
                     f"ws://{self.ip}:{self.port}",
                     ping_interval=None,
                     close_timeout=self.timeout_connect,
+                    max_size=None,
                 ),
                 timeout=self.timeout_connect,
             )
